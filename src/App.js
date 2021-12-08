974af19661cb35785cbf4240fc99768a17eb43cc
David Moranchel <david.arcshtag@gmail.com>
2021-12-07 21:34:38 -0600

    feat: first GitHub commit :octocat:

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+// Components
+// import Greeting from "./components/Greeting";
+import Calculator from "./components/Calculator";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+
+				<Calculator firstValue={1} secondValue={2} operator={"suma"} />
+				<Calculator
+					firstValue={4}
+					secondValue={2}
+					operator={"multiplicacion"}
+				/>
+				<Calculator firstValue={10} secondValue={2} operator={"restass"} />
+			</header>
+		</div>
+	);
 }
 
 export default App;
