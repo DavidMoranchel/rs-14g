1caa26c46a886b2c3c2a683e5f2ea04b697040e6
David Moranchel <david.arcshtag@gmail.com>
2021-12-09 20:41:19 -0600

    feat: NavBar completed

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function NavBar() {
+// Styles
+import "./NavBar.css";
+
+function NavBar() {
+	const [active, setActive] = useState(3);
+
+	const handleClick = (itemID) => {
+		setActive(itemID);
+	};
+
+	const checkActive = (itemID) => (active === itemID ? "li-active" : "");
+
 	return (
 		<div>
 			<ul>
-				<li>Item 1</li>
-				<li>Item 2</li>
-				<li>Item 3</li>
+				<li className={checkActive(1)} onClick={(e) => handleClick(1)}>
+					Item 1
+				</li>
+				<li className={checkActive(2)} onClick={() => handleClick(2)}>
+					Item 2
+				</li>
+				<li className={checkActive(3)} onClick={() => handleClick(3)}>
+					Item 3
+				</li>
 			</ul>
 		</div>
 	);
 }
+
+export default NavBar;
