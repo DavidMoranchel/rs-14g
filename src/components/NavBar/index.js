728573d1e13e4c0e38eedccdddaaf9b8003772d7
David Moranchel <david.arcshtag@gmail.com>
2021-12-09 21:45:19 -0600

    feat: NavBar content completed

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -11,11 +11,28 @@ function NavBar() {
 	};
 
 	const checkActive = (itemID) => (active === itemID ? "li-active" : "");
+	// const checkActiveContent = (itemID) => (active === itemID ? true : false);
+
+	let content = "";
+	switch (active) {
+		case 1:
+			content = "Holi 1";
+			break;
+		case 2:
+			content = "Holi 2";
+			break;
+		case 3:
+			content = "Holi 3";
+			break;
+		default:
+			content = "No holi";
+			break;
+	}
 
 	return (
 		<div>
 			<ul>
-				<li className={checkActive(1)} onClick={(e) => handleClick(1)}>
+				<li className={checkActive(1)} onClick={() => handleClick(1)}>
 					Item 1
 				</li>
 				<li className={checkActive(2)} onClick={() => handleClick(2)}>
@@ -24,7 +41,23 @@ function NavBar() {
 				<li className={checkActive(3)} onClick={() => handleClick(3)}>
 					Item 3
 				</li>
+				<li className={checkActive(4)} onClick={() => handleClick(4)}>
+					Item 4
+				</li>
 			</ul>
+			<div>
+				<h1>{content}</h1>
+			</div>
+			{/* <div>
+				{checkActiveContent(1) ? <h1>Holi 1</h1> : null}
+				{checkActiveContent(2) ? <h1>Holi 2</h1> : null}
+				{checkActiveContent(3) ? <h1>Holi 3</h1> : null}
+			</div> */}
+			{/* <div>
+				<h1 className={checkActiveContent(1)}>Holi 1</h1>
+				<h1 className={checkActiveContent(2)}>Holi 2</h1>
+				<h1 className={checkActiveContent(3)}>Holi 3</h1>
+			</div> */}
 		</div>
 	);
 }
