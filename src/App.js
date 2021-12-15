4c456c2ef3c83e20d61dd1422da268c8c7c36333
David Moranchel <david.arcshtag@gmail.com>
2021-12-14 21:00:36 -0600

    feat: implement prev and isLoading logic

--- src/App.js
+++ src/App.js
@@ -10,33 +10,57 @@ import { listCharacters } from "./services/characters";
 function App() {
 	const [characters, setCharacters] = useState([]);
 	const [data, setData] = useState({});
+	const [isLoading, setIsLoading] = useState(true);
 
 	useEffect(() => {
 		const list = async () => {
 			const { results, info } = await listCharacters();
 			setCharacters(results);
 			setData(info);
+			setIsLoading(false);
 		};
 		list();
 	}, []);
 
 	const handleClick = async (action) => {
+		setIsLoading(true);
+		let page;
 		if (action === "next" && data.next != null) {
-			const page = data.next.split("?")[1];
-			const { results, info } = await listCharacters(page);
-			setCharacters(results);
-			setData(info);
+			page = data.next.split("?")[1];
 		}
+		if (action === "prev" && data.prev != null) {
+			page = data.prev.split("?")[1];
+		}
+		const { results, info } = await listCharacters(page);
+		setCharacters(results);
+		setData(info);
+		setIsLoading(false);
 	};
 
-	console.log(data);
+	const hasPrevLink = data.prev ? true : false;
+	const hasNextLink = data.next ? true : false;
 
 	return (
 		<div className="App">
 			<div className="fixed-container">
-				<button onClick={() => handleClick("next")} className="btn">
-					Next
-				</button>
+				{hasPrevLink ? (
+					<button
+						disabled={isLoading}
+						onClick={() => handleClick("prev")}
+						className="btn"
+					>
+						Prev
+					</button>
+				) : null}
+				{hasNextLink ? (
+					<button
+						disabled={isLoading}
+						onClick={() => handleClick("next")}
+						className="btn"
+					>
+						Next
+					</button>
+				) : null}
 			</div>
 			{characters.map(({ id, image, name, species, status }) => (
 				<Character
