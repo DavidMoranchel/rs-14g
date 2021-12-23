2499904668e493207f5b175158359faa8cf2f41f
David Moranchel <david.arcshtag@gmail.com>
2021-12-22 21:36:02 -0600

    feat: state, events and life cycle

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,11 +2,13 @@ import React from "react";
 
 // Components
 import Greet from "../../components/Greet";
+import Counter from "../../components/Counter";
 
 export default function Home() {
 	return (
 		<div>
 			<Greet firstName="Fego" />
+			<Counter />
 		</div>
 	);
 }
